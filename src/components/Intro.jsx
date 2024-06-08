0a4694981b3c098baaf9260c6d2c8a9c0ec4c562
minse317 <s2019w30@e-mirim.hs.kr>
2024-06-08 23:48:30 +0900

    intro page

---
+++ src/components/Intro.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <div className='h-screen font-libre-baskerbille text-9xl relative'>
+      <div className='p-20'>BAEK MINSE</div>
+      <div className='absolute right-0 bottom-0 p-20'>PORTFOLIO</div>
+    </div>
+  );
+}
+
+export default Main;

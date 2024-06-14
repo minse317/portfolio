f46a00239024a8c75b2744e7ed929ac3e4e4651e
minse317 <s2019w30@e-mirim.hs.kr>
2024-06-14 22:52:07 +0900

    Contact 행간 수정 및 Project 폰트 수정

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,15 +5,16 @@ function Project() {
   return (
     <div className='p-20'>
       <div className='text-5xl font-bold'>Project</div>
+
       <div className='flex flex-wrap'>
       {project.map((item, index) => (
-        <div key={index} className="w-[430px] border-b-4 border-[black] bg-transparent m-5 p-5">
+        <div key={index} className="w-[430px] border-b-4 border-[black] bg-transparent m-5 p-5 space-y-2">
           <h2 className='mb-5'>{item.title}</h2>
           <p>{item.desc}</p>
           <p>{item.period}</p>
           <p>{item.skill}</p>
-          <a href={item.link}>GitHub</a>
           <p>{item.role}</p>
+          <p><a href={item.link} target='_blank'>GitHub</a></p>
         </div>
       ))}
       </div>

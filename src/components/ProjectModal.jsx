ebf03dd51a55102169faf4c7fcbceabc4491e848
minse317 <s2019w30@e-mirim.hs.kr>
2024-06-22 01:10:01 +0900

    모달창 수정

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -13,23 +13,43 @@ function ProjectModal({ project, onClose }) {
       onClick={handleOnClose}
       className="fixed inset-0 bg-black bg-opacity-20 backdrop-blur-sm flex justify-center items-center"
     >
-      <div className="bg-white p-10 rounded w-2/6 h-auto overflow-y-auto">
-        <h2 className="mb-5">{project.title}</h2>
-        <div className="space-y-4">
+      <div className="bg-white p-10 rounded w-2/6 h-auto">
+        <div className="flex items-baseline mb-5">
+          <h2 className="mr-4">{project.title}</h2>
+          <p>{project.type}</p>
+        </div>
+
+        <div className="space-y-2">
           <p>{project.desc}</p>
+          <div className="flex">
+            <p className="mr-5">
+              <a
+                href={project.link}
+                className="underline"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+            </p>
+            {project.presentation && (
+              <p>
+                <a
+                  href={project.presentation}
+                  className="underline"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  발표 자료
+                </a>
+              </p>
+            )}
+          </div>
           <p>{project.period}</p>
           <p>{project.skill}</p>
           <p>{project.role}</p>
-          <p>
-            <a
-              href={project.link}
-              className="underline"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              GitHub
-            </a>
-          </p>
+          <h4>느낀 점</h4>
+          <p className="whitespace-pre-wrap pt-0">{project.review}</p>
         </div>
       </div>
     </div>
